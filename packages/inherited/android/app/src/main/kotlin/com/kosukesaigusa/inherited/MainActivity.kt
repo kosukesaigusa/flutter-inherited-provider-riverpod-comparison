d13536a83d3668c3c971d02228c98e992300850c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-01-11 17:52:33 +0900

    first commit

---
+++ packages/inherited/android/app/src/main/kotlin/com/kosukesaigusa/inherited/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kosukesaigusa.inherited
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/provider/android/app/src/main/kotlin/com/kosukesaigusa/provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kosukesaigusa.provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
