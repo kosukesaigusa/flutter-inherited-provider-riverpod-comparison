a20c6adb14600e5a657747c02f2718a9534c7903
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-01-18 14:35:53 +0900

    test: add TodoModel addTodo method test

--- packages/provider/test/features/todo/todo_test.dart
+++ packages/provider/test/features/todo/todo_test.dart
@@ -8,22 +8,35 @@ import 'todo_test.mocks.dart';
 
 @GenerateMocks([TodoRepository])
 void main() {
-  late final TodoModel todoModel;
   late final TodoRepository todoRepository;
 
   setUpAll(() {
     todoRepository = MockTodoRepository();
-    todoModel = TodoModel(todoRepository: todoRepository);
   });
 
   group('TodoModel のテスト。', () {
     test('fetchTodos メソッドのテスト。', () async {
+      final todoModel = TodoModel(todoRepository: todoRepository);
       expect(todoModel.todos, const <Todo>[]);
       final expected = [_FakeTodo(), _FakeTodo()];
       when(todoRepository.fetchTodos())
           .thenAnswer((_) => Future<List<Todo>>.value(expected));
       await todoModel.fetchTodos();
       expect(todoModel.todos, expected);
+    });
+
+    test('addTodo メソッドのテスト。', () async {
+      final todoModel = TodoModel(todoRepository: todoRepository);
+      final todos = [_FakeTodo(), _FakeTodo()];
+      for (final todo in todos) {
+        when(
+          todoRepository.addTodo(
+            todo,
+          ),
+        ).thenAnswer((_) => Future<void>.value());
+        await todoModel.addTodo(todo);
+      }
+      expect(todoModel.todos, todos);
     });
   });
 }
